f051bd861e0e47adc16187e8c20f8dac12740526
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2023-06-08 16:05:25 +0000

    Deploying to gh-pages from @ Ivankimachuk/goit-js-hw-09@8249aa84796397383f5175f0afe6f5aa21d9d08c 🚀

---
+++ 01-color-switcher.04c67b62.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");let o=null;function a(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;n.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o=setInterval(a,1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0})),t.style.fontSize="30px",e.style.fontSize="30px",t.style.margin="10px";
+//# sourceMappingURL=01-color-switcher.04c67b62.js.map
